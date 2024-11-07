661a662f296e3075c6299a63405aec90d33159ab
Hernan <hernanceron@gmail.com>
2024-11-07 15:16:09 -0500

    agregando el destroy

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.8.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name = "my-terraform-state-rg"
+    storage_account_name = "mytfstatesahca"
+    container_name = "tfstate"
+    key = "dev/terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
