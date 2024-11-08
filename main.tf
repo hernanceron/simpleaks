1b49704b3f3190560822fcf8b0d89057f34970da
Hernan <hernanceron@gmail.com>
2024-11-08 11:48:22 -0500

    otra actualizacion

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "azurerm_resource_group" "default" {
 # Creando virtual net
 resource "azurerm_virtual_network" "vnetsample" {
   name = "${random_pet.prefix.id}-vnet"
-  address_space = ["10.1.0.0/16"]
+  address_space = ["10.0.0.0/16"]
   location = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
 }
